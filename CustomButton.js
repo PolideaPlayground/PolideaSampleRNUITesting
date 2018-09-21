4b932d9cf8f1ff91f49621019facf69536879798
Adam Stasiak <adam.stasiak@polidea.com>
2018-09-21 12:42:50 +0300

    Updated project to use detox 9

--- CustomButton.js
+++ CustomButton.js
@@ -16,24 +16,24 @@ class CustomButton extends Component {
 	render() {
 		const { text, onPress} = this.props;
 		return (
-			
+
 			<TouchableOpacity  style={styles.buttonStyle}
-			onPress={() => this.setState({clicked:!this.state.clicked})}>	
-			
-			 <Text 
+			onPress={() => this.setState({clicked:!this.state.clicked})}>
+
+			 <Text
 			 	accessible= {true}
 			 	testID= {"ButtonText"}
 			 	accessibilityLabel= {"ButtonTextDesc"}
 			 	style={styles.textStyle}>
 
 				 {!this.state.clicked? defaultButtonText: "Polidea"}
-				 
+
 			 </Text>
 			 <Image
-					accessible={true} 
+					accessible={true}
 					testID='ButtonImage'
           style={!this.state.clicked ? styles.imageStyleDefault : styles.imageStyle}
-          source={{uri:'https://www.polidea.com/img/polidea-new-logo.png'}}
+          source={{uri:'https://pbs.twimg.com/profile_images/580677325974536192/oP6A55eF_400x400.png'}}
           />
 		  </TouchableOpacity>
 		);
@@ -52,14 +52,14 @@ const styles = StyleSheet.create({
 	color: '#ffffff',
 	textAlign: 'center'
   },
-  
+
   buttonStyle: {
 	padding:10,
 	backgroundColor: '#209DD8',
 	borderRadius:10,
 	},
 	imageStyle: {
-		width: 150, 
+		width: 150,
 		height: 150,
 		backgroundColor: "#209DD8",
 		borderRadius:10,
@@ -67,14 +67,14 @@ const styles = StyleSheet.create({
     alignItems: 'center'
 	},
 	imageStyleDefault: {
-		width: 0, 
+		width: 0,
 		height: 0,
 		backgroundColor: "#209DD8",
 		borderRadius:10,
 		justifyContent: 'center',
     alignItems: 'center'
 	}
-	
+
 });
 
 export default CustomButton;
